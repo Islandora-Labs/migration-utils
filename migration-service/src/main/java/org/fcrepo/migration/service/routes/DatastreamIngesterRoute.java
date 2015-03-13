1a84313ce822720beed9e9a13f5b7bd24eb0da37
Daniel Lamb <daniel@discoverygarden.ca>
2015-03-13 21:48:15 +0000

    Horrible cp command screw-up when attempting to deploy resulted in a duplicate file.  Thank God for undo-tree in vim.

--- migration-service/src/main/java/org/fcrepo/migration/service/routes/DatastreamIngesterRoute.java
+++ migration-service/src/main/java/org/fcrepo/migration/service/routes/DatastreamIngesterRoute.java
@@ -1,17 +1,40 @@
 package org.fcrepo.migration.service.routes;
 
+import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.builder.xml.Namespaces;
+import org.apache.camel.component.http.HttpMethods;
+import org.fcrepo.camel.FcrepoHeaders;
 
-public class FoxmlMigratorRoute extends RouteBuilder {
+public class DatastreamIngesterRoute extends RouteBuilder {
     public void configure() throws Exception {
-        // Configure rest servlet
-        restConfiguration().component("servlet");
+        // Namespaces
+        Namespaces ns = new Namespaces("foxml", "info:fedora/fedora-system:def/foxml#");
+        ns.add("audit", "info:fedora/fedora-system:def/audit#");
 
-        // Set up endpoints
-        rest("/migrate")
-            .description("Migrates an object to Fedora4 from a FOXML file.")
+        from("direct:ingestDatastream")
+            // Get the mimetype
+            .setProperty("mimetype").xpath("/foxml:datastream/foxml:datastreamVersion/@MIMETYPE", ns)
+            .log("MIMETYPE ${property.mimetype}")
 
-            .post("/foxml")
-                .to("direct:ingestContainer");
+            // Set headers
+            .setHeader(FcrepoHeaders.FCREPO_IDENTIFIER).simple("${property.container}")
+            .setHeader(Exchange.CONTENT_TYPE).simple("${property.mimetype}")
+            .setHeader(Exchange.HTTP_METHOD, constant(HttpMethods.POST))
+
+            // Switch statement on mimetype and xml/binary
+            .choice()
+                // Add RDF properties here
+                .when().simple("${property.mimetype} == 'application/rdf+xml'")
+                    .log("You should add RDF properties to the parent container here!")
+                // Add xml content as NonRDFSourceDescription
+                .when().xpath("/foxml:datastream/foxml:datastreamVersion/foxml:xmlContent", ns)
+                    .setBody().xpath("/foxml:datastream/foxml:datastreamVersion/foxml:xmlContent", ns)
+                    .to("fcrepo:localhost:8080/fcrepo/rest")
+                // Add binary content as NonRDFSourceDescription
+                .when().xpath("/foxml:datastream/foxml:datastreamVersion/foxml:binaryContent", ns)
+                    .setBody().xpath("/foxml:datastream/foxml:datastreamVersion/foxml:binaryContent/text()", ns).unmarshal().base64()
+                    .to("fcrepo:localhost:8080/fcrepo/rest");
+            
     }
 }
