1173d221e3de1c60cfaf746a3fdcef4269e9d075
Daniel Lamb <daniel@discoverygarden.ca>
2015-03-30 19:04:01 +0000

    Splitting on datastream version.

--- src/main/java/org/fcrepo/migration/routes/DatastreamIngester.java
+++ src/main/java/org/fcrepo/migration/routes/DatastreamIngester.java
@@ -1,10 +1,7 @@
 package org.fcrepo.migration.routes;
 
-import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.builder.xml.Namespaces;
-import org.fcrepo.camel.FcrepoHeaders;
-import org.fcrepo.camel.HttpMethods;
 
 /**
  * Ingests a datastream from Fedora 3 foxml as a NonRdfSourceDescription in 
@@ -21,30 +18,7 @@ public class DatastreamIngester extends RouteBuilder {
         ns.add("audit", "info:fedora/fedora-system:def/audit#");
 
         from("seda:datastream")
-            //TODO: Handle each version of this particular datastream
-
-            // Get the mimetype
-            .setProperty("mimetype").xpath("/foxml:datastream/foxml:datastreamVersion/@MIMETYPE", ns)
-            .log("MIMETYPE ${property.mimetype}")
-
-            // Set headers
-            .setHeader(FcrepoHeaders.FCREPO_IDENTIFIER).simple("${property.container}")
-            .setHeader(Exchange.CONTENT_TYPE).simple("${property.mimetype}")
-            .setHeader(Exchange.HTTP_METHOD, constant(HttpMethods.POST))
-
-            // Switch statement on mimetype and xml/binary
-            .choice()
-                // Add RDF properties here
-                .when().simple("${property.mimetype} == 'application/rdf+xml'")
-                    .log("You should add RDF properties to the parent container here!")
-                // Add xml content as NonRDFSourceDescription
-                .when().xpath("/foxml:datastream/foxml:datastreamVersion/foxml:xmlContent", ns)
-                    .setBody().xpath("/foxml:datastream/foxml:datastreamVersion/foxml:xmlContent", ns)
-                    .to("fcrepo:{{fcrepo.baseurl}}")
-                // Add binary content as NonRDFSourceDescription
-                .when().xpath("/foxml:datastream/foxml:datastreamVersion/foxml:binaryContent", ns)
-                    .setBody().xpath("/foxml:datastream/foxml:datastreamVersion/foxml:binaryContent/text()", ns)
-                    .unmarshal().base64()
-                    .to("fcrepo:{{fcrepo.baseurl}}");
+            .split().xpath("/foxml:datastream/foxml:datastreamVersion")
+            .to("seda:datastreamVersion");
     }
 }
---
+++ src/main/java/org/fcrepo/migration/routes/DatastreamVersionIngester.java
@@ -0,0 +1,48 @@
+package org.fcrepo.migration.routes;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.builder.xml.Namespaces;
+import org.fcrepo.camel.FcrepoHeaders;
+import org.fcrepo.camel.HttpMethods;
+
+/**
+ * Ingests a datastream from Fedora 3 foxml as a NonRdfSourceDescription in 
+ * Fedora 4.
+ *
+ * @author Daniel Lamb
+ */
+public class DatastreamVersionIngester extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+        // Namespaces
+        final Namespaces ns = new Namespaces("foxml", "info:fedora/fedora-system:def/foxml#");
+        ns.add("audit", "info:fedora/fedora-system:def/audit#");
+
+        from("seda:datastreamVersion")
+            // Get the mimetype
+            .setProperty("mimetype").xpath("/foxml:datastreamVersion/@MIMETYPE", ns)
+            .log("MIMETYPE ${property.mimetype}")
+
+            // Set headers
+            .setHeader(FcrepoHeaders.FCREPO_IDENTIFIER).simple("${property.container}")
+            .setHeader(Exchange.CONTENT_TYPE).simple("${property.mimetype}")
+            .setHeader(Exchange.HTTP_METHOD, constant(HttpMethods.POST))
+
+            // Switch statement on mimetype and xml/binary
+            .choice()
+                // Add RDF properties here
+                .when().simple("${property.mimetype} == 'application/rdf+xml'")
+                    .log("You should add RDF properties to the parent container here!")
+                // Add xml content as NonRDFSourceDescription
+                .when().xpath("/foxml:datastreamVersion/foxml:xmlContent", ns)
+                    .setBody().xpath("/foxml:datastreamVersion/foxml:xmlContent", ns)
+                    .to("fcrepo:{{fcrepo.baseurl}}")
+                // Add binary content as NonRDFSourceDescription
+                .when().xpath("/foxml:datastreamVersion/foxml:binaryContent", ns)
+                    .setBody().xpath("/foxml:datastreamVersion/foxml:binaryContent/text()", ns)
+                    .unmarshal().base64()
+                    .to("fcrepo:{{fcrepo.baseurl}}");
+    }
+}
